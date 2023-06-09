0329960eea88e3b65c7eac10fcba63ade9217b3c
lv <992526373@qq.com>
2023-06-09 11:37:01 +0800

    feat：Login

--- src/pages/auth/index.tsx
+++ src/pages/auth/index.tsx
@@ -1,8 +1,20 @@
+import AuthModal from "@/components/Modals/AuthModal";
+import Navbar from "@/components/Navbar/Navbar";
 import React from "react";
 
 type AuthPageProps = {};
 
 const index: React.FC<AuthPageProps> = () => {
-  return <div>Auth Page</div>;
+  return (
+    <div className="bg-gradient-to-b from-gray-600 to-black h-screen relative">
+      <div className="max-w-7xl mx-auto">
+        <Navbar />
+        <div className="flex items-center justify-center h-[calc(100vh-5rem)] pointer-events-none select-none">
+          <img src="/vercel.svg" alt="img" />
+        </div>
+        <AuthModal />
+      </div>
+    </div>
+  );
 };
 export default index;
