b73f92d30ea0e4595111d6a52fd443a6dad607c9
lv <992526373@qq.com>
2023-06-12 16:09:53 +0800

    feat：问题页面

---
+++ src/pages/problems/[pid].tsx
@@ -0,0 +1,13 @@
+import Topbar from "@/components/Topbar/Topbar";
+import React from "react";
+
+type problemPageProps = {};
+
+const problemPage: React.FC<problemPageProps> = () => {
+  return (
+    <div>
+      <Topbar problemPage />
+    </div>
+  );
+};
+export default problemPage;
