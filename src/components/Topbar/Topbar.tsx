8c86fc37e8d28b82bea754f33a46f7fc9f98f0cf
lv <992526373@qq.com>
2023-06-12 15:38:22 +0800

    feat：问题表 UI

--- src/components/Topbar/Topbar.tsx
+++ src/components/Topbar/Topbar.tsx
@@ -1,9 +1,20 @@
 import Link from "next/link";
 import React from "react";
+import Image from "next/image";
+import Logout from "../Buttons/Logout";
+import { useSetRecoilState } from "recoil";
+import { authModalState } from "@/atoms/authModalAtom";
 
 type TopbarProps = {};
 
 const Topbar: React.FC<TopbarProps> = () => {
+  const user = {
+    name: "user",
+    email: "123456@email.com",
+  };
+
+  const setAuthModalState = useSetRecoilState(authModalState);
+
   return (
     <nav className="relative flex h-[50px] w-full shrink-0 items-center px-5 bg-dark-layer-1 text-dark-gray-7">
       <div
@@ -24,11 +35,42 @@ const Topbar: React.FC<TopbarProps> = () => {
               Premium
             </a>
           </div>
-          <Link href="/auth">
-            <button className="bg-dark-fill-3 py-1 px-2 cursor-pointer rounded ">
-              Sign In
-            </button>
-          </Link>
+          {!user && (
+            <Link
+              href="/auth"
+              onClick={() =>
+                setAuthModalState((prev) => ({
+                  ...prev,
+                  isOpen: true,
+                  type: "login",
+                }))
+              }
+            >
+              <button className="bg-dark-fill-3 py-1 px-2 cursor-pointer rounded ">
+                Sign In
+              </button>
+            </Link>
+          )}
+
+          {user && (
+            <div className="cursor-pointer group relative">
+              <Image
+                src="/avatar.png"
+                alt="Avatar"
+                width={30}
+                height={30}
+                className="rounded-full"
+              />
+              <div
+                className="absolute top-10 left-2/4 -translate-x-2/4  mx-auto bg-dark-layer-1 text-brand-orange p-2 rounded shadow-lg 
+								z-40 group-hover:scale-100 scale-0 
+								transition-all duration-300 ease-in-out"
+              >
+                <p className="text-sm">{user.email}</p>
+              </div>
+            </div>
+          )}
+          {user && <Logout />}
         </div>
       </div>
     </nav>
